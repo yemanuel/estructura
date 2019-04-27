95e77c0a8bb5d1856df4b75c5b5937925fdd0db4
yemanuel <gutierrezvasquezyimi@gmail.com>
2019-04-26 19:48:57 -0500

    Revert "Update ESTRUTURA.php"
    This reverts commit 22d565d5cc18fda8c761bbbdd45e18373affc6b8.

--- ESTRUTURA.php
+++ ESTRUTURA.php
@@ -3,45 +3,15 @@
     $nombres;
 
     //PHP-ES-UN-LENGUAJE-DE-EXPRESIONES
-  
-  /*  if($nombres)
+    if($nombres)
     {
 
        echo"este mensaje es si la expresion devuelve true";
        echo "<br />Linea 2 :este mensaje es si la expresion devuelve true";
       }
-*/
 
-?>
-<table>
-	<tr>
-		
-		<th>nombre</th>
-		<th>apellidos</th>
-		<th>dni</th>
-	</tr>
 
-	<?php if (1) : ?>
-		<style type="text/css">
-			
-			td{
-				background: green;
-				color: white;
-			}
 
-		</style>
-		<tr>
-			<td>luis</td>
-			<td>gutierrez</td>
-			<td>12345678</td>
-		</tr>
-		<tr>
-			<td>juan </td>
-			<td>vasquez</td>
-			<td>52634178</td>
-		</tr>
-	<?php endif;?>
-</table>
 
 
 
