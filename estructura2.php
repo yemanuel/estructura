97e479ddc98d63b8b8a6d19ae01cbd25ac2c3823
yemanuel <gutierrezvasquezyimi@gmail.com>
2019-04-26 20:02:23 -0500

    estructura
    problema

--- estructura2.php
+++ estructura2.php
@@ -7,3 +7,24 @@ if (true)
 {
 	echo"mensaje por ser False";
 }
+echo "<br />";
+echo False?"mensaje por ser true":"mensaje por ser False";
+
+?>
+<?php
+$ monto;// indicar si monto tiene descuento      (monto >1000)
+$ edad;// indicar si es mayor o menor de edad
+$ sueldo; //indicar si el sueldo es minimo o no
+
+if (true)
+ {
+	echo"mensaje por ser true" ;
+
+} else
+{
+	echo"mensaje por ser False;
+}
+echo "<br / >";
+echo false ?"mensaje por ser true";"mensaje por ser false";
+
+?>
