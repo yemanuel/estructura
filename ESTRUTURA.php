9c8ed83f3562ebffc9f8fc037745eb29a029b7bb
yemanuel <gutierrezvasquezyimi@gmail.com>
2019-04-26 19:17:56 -0500

    es

---
+++ ESTRUTURA.php
@@ -0,0 +1,22 @@
+<?php
+    $cantidad=0;
+    $nombres;
+
+    //PHP-ES-UN-LENGUAJE-DE-EXPRESIONES
+    if($nombres)
+    {
+
+       echo"este mensaje es si la expresion devuelve true";
+       echo "<br />Linea 2 :este mensaje es si la expresion devuelve true";
+      }
+
+
+
+
+
+
+
+
+
+
+   ?>
